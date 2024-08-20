053db0b97efbb22d5a0a8dc445beb04293a09b8d
Nick Savage <nick@nicksavage.ca>
2024-08-20 06:38:35 -0400

    Add beginnings of map

--- src/stdlib/arrays.go
+++ src/stdlib/arrays.go
@@ -1,6 +1,7 @@
 package stdlib
 
 import (
+	//	"errors"
 	"fmt"
 	"github.com/NickSavage/glox/src/interpreter"
 	"github.com/NickSavage/glox/src/parser"
@@ -38,4 +39,64 @@ func lenImplementation(i interface{}) (interface{}, error) {
 	}
 	log.Printf("a %v", a)
 	return a.Length, nil
+}
+
+func MapFunction(i *interpreter.Interpreter) *parser.Statement {
+	result := &parser.Statement{
+		Type:         tokens.TokenType{Type: "NativeFunction"},
+		FunctionName: identifierToken("len"),
+		Parameters: []tokens.Token{
+			identifierToken("array"),
+			identifierToken("func"),
+		},
+		NativeFunction: mapImplementation,
+	}
+	return result
+
+}
+func mapImplementation(i interface{}) (interface{}, error) {
+	in, ok := i.(*interpreter.Interpreter)
+
+	if !ok {
+		return nil, implWrapperError()
+	}
+	a, _ := in.Memory.Get("array")
+	array, ok := a.(*parser.Array)
+	if !ok {
+		log.Printf("error")
+		return nil, fmt.Errorf("value is not an array")
+	}
+	f, _ := in.Memory.Get("func")
+	function, ok := f.(*parser.Statement)
+	if !ok {
+		log.Printf("error")
+		return nil, fmt.Errorf("function needs to be a function")
+	}
+
+	var result []tokens.Token
+	var elementResult interface{}
+	var rerr interpreter.RuntimeError
+
+	log.Printf("asdsa %v", function.Expression)
+	var arguments []interface{}
+	for _, e := range array.Elements {
+		// need to evaluate here
+		arguments = make([]interface{}, 0)
+		arguments = append(arguments, e)
+
+		elementResult, rerr = in.FunctionCall(function.Expression, arguments)
+		if rerr.HasError {
+			log.Printf("map rerr %v", rerr.Message)
+			return nil, rerr.Message
+		}
+		result = append(result, tokens.Token{
+			Type:    e.Type,
+			Lexeme:  fmt.Sprintf("%v", elementResult),
+			Line:    e.Line,
+			Literal: elementResult,
+		})
+
+	}
+
+	return array, nil
 }
--- src/stdlib/arrays_test.go
+++ src/stdlib/arrays_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/NickSavage/glox/src/interpreter"
 	"github.com/NickSavage/glox/src/parser"
 	"github.com/NickSavage/glox/src/tokens"
+	//"log"
 	"testing"
 )
 
@@ -77,3 +78,46 @@ func TestNativeLen(t *testing.T) {
 		t.Errorf("wrong result, got %v want %v", result, 4)
 	}
 }
+
+// func TestNativeMap(t *testing.T) {
+
+// 	memory := &interpreter.Storage{
+// 		Memory: make(map[string]interface{}),
+// 	}
+
+// 	text := " var a = [1,2,3,4];\n func double(x) { return x * 2; }\n var b = map(a, double);"
+// 	declarations, err := parseDeclarations(t, text)
+// 	if err != nil {
+// 		t.Errorf(err.Error())
+// 	}
+// 	if len(declarations) != 3 {
+// 		t.Errorf("wrong number of declarations, got %v want %v", len(declarations), 3)
+// 	}
+// 	i := interpreter.Interpreter{
+// 		Expression: declarations[0].Expression,
+// 		Memory:     memory,
+// 	}
+// 	i.Memory.Define("map", MapFunction(&i))
+// 	for _, declaration := range declarations {
+// 		i.Expression = declaration.Expression
+// 		rerr := i.Execute(declaration)
+// 		if rerr.HasError {
+// 			t.Errorf(rerr.Message.Error())
+// 		}
+// 	}
+// 	result, err := i.Memory.Get("b")
+// 	log.Printf("result %v", result)
+// 	if err != nil {
+// 		t.Errorf(err.Error())
+// 	}
+// 	r, ok := result.(*parser.Array)
+// 	if !ok {
+// 		t.Errorf("wrong type returned, got %T want %T", result, r)
+// 	}
+// 	if r.Length != 4 {
+// 		t.Errorf("wrong number of elements returned, got %v want %v", r.Length, 4)
+// 	}
+// 	if r.Elements[0].Literal != 2 {
+// 		t.Errorf("wrong result, got %v want %v", r.Elements[0].Literal, 2)
+// 	}
+// }
